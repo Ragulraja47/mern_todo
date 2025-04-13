26a44afdd95f9cfc965c8b8774062916c9871b81
Ragulraja47 <cragulraja2004@gmail.com>
2025-04-13 15:39:37 +0530

    fgh

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const {createTODO} = require('../models/todo.js');
 const router = express.Router();
+
+router.post('/',async(req,res)=>{
+    console.log({body:req.body});
+    const {title,description} = req.body;
+    const todo = await createTODO(title,description);
+    res.send({todo});
+})
 
 module.exports = router;
