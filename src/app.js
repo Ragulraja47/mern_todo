b551fdf49adfbd929e2d154fb463b28a1625bd06
Ragulraja47 <cragulraja2004@gmail.com>
2025-04-13 14:57:28 +0530

    fgh

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const todorouter = require('./routes/todo.js');
+const app = express();
+const port = 3000;
+
+app.use('/todo',todorouter);
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
